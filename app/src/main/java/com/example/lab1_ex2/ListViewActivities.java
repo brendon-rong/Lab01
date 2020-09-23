7b052f5eaa8fe1158283da109c6ef72503945dcb
Bmjr1213 <54966783+Bmjr1213@users.noreply.github.com>
2020-09-23 12:33:32 -0400

    minor changes

--- app/src/main/java/com/example/lab1_ex2/ListViewActivities.java
+++ app/src/main/java/com/example/lab1_ex2/ListViewActivities.java
@@ -1,15 +1,19 @@
 package com.example.lab1_ex2;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 /**
  * A fragment representing a list of Items.
@@ -30,8 +34,21 @@ public class ListViewActivities extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_list_view_activities, container, false);
 
-        ListView listView = view.findViewById(R.id.listView);
+        final ListView listView = view.findViewById(R.id.listView);
 
+        listView.setOnItemClickListener(new android.widget.AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,int position, long id) {
+                String item = (String) listView.getItemAtPosition(position);
+                String tag = "poop";
+                Log.d(tag, item + " selected");
+
+                if(item.equalsIgnoreCase("AIActivity")){
+
+                }
+            }
+        });
+        
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, activity);
 
         listView.setAdapter(adapter);
